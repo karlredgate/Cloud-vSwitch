29b5c07fd60bd40dac00c399a2e3c9f525c9275d
SiyanH <46579757+SiyanH@users.noreply.github.com>
2021-03-10 22:34:08 -0500

    Create app main page

--- web/src/App.js
+++ web/src/App.js
@@ -3,21 +3,20 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img src={logo} className="app-logo" alt="logo" />
+      <h1 className="is-size-1 has-text-weight-semibold is-family-monospace mt-5 mb-3">
+        Cloud vSwitch
+      </h1>
+      <div className="buttons">
+        <button className="button is-info mr-3">Sign In</button>
+        <button className="button is-warning">
+          <span className="icon">
+            <i className="fab fa-github"></i>
+          </span>
+          <span>GitHub</span>
+        </button>
+      </div>
     </div>
   );
 }
