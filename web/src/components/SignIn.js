5d32fd8f85cc096a529cb5c58d36ff8bd3c21409
SiyanH <46579757+SiyanH@users.noreply.github.com>
2021-03-11 12:13:14 -0500

    Create layout for Sign In page

--- web/src/components/SignIn.js
+++ web/src/components/SignIn.js
@@ -1,5 +1,38 @@
+import "./SignIn.css";
+import { Link } from "react-router-dom";
+
 function SignIn() {
-  return <h1>Sign In</h1>;
+  return (
+    <div className="sign-in">
+      <h1 className="title">Sign In</h1>
+      <div className="field">
+        <label className="label">Email</label>
+        <div className="control has-icons-left">
+          <input
+            className="input"
+            type="email"
+            placeholder="cloud@example.com"
+          />
+          <span className="icon is-small is-left">
+            <i className="fas fa-envelope"></i>
+          </span>
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Password</label>
+        <div className="control has-icons-left">
+          <input className="input" type="password" />
+          <span className="icon is-small is-left">
+            <i className="fas fa-lock"></i>
+          </span>
+        </div>
+      </div>
+      <div className="action-row mt-1">
+        <button className="button is-info">Sign in</button>
+        <Link to="">Sign up</Link>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
