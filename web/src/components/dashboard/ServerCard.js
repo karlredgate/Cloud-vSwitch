d665293561d7efe783465983658c6b04289027fa
SiyanH <46579757+SiyanH@users.noreply.github.com>
2021-03-11 22:46:42 -0500

    Create server card layout

---
+++ web/src/components/dashboard/ServerCard.js
@@ -0,0 +1,42 @@
+import "./ServerCard.css";
+
+function ServerCard() {
+  return (
+    <div className="card server-card">
+      <header className="card-header">
+        <p className="card-header-title">Server</p>
+        <div className="is-flex px-2">
+          <div className="card-header-icon" role="button">
+            <span className="icon">
+              <i className="fas fa-play"></i>
+            </span>
+          </div>
+          <div className="card-header-icon" role="button">
+            <span className="icon">
+              <i className="fas fa-pen"></i>
+            </span>
+          </div>
+        </div>
+      </header>
+      <div className="card-content">
+        <div className="content">
+          <p>
+            <span className="has-text-weight-semibold">Status: </span>
+            <span className="icon has-text-success is-small">
+              <i className="fas fa-circle fa-xs"></i>
+            </span>{" "}
+            Running
+          </p>
+          <p>
+            <span className="has-text-weight-semibold">Organization: </span>
+            CS6620
+          </p>
+          <p className="has-text-weight-semibold">Description: </p>
+          <p>Test server</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ServerCard;
