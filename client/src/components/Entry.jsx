0f92c904b02bc047a2893f6ff5b1683704cd9c67
SiyanH <46579757+SiyanH@users.noreply.github.com>
2021-03-24 23:03:21 -0400

    Create entry page

---
+++ client/src/components/Entry.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import styles from './Entry.css';
+import SignIn from './SignIn';
+
+function Entry() {
+  return (
+    <div className={styles.app}>
+      <img src={logo} className={styles.appLogo} alt="logo" />
+      <h1 className="is-size-1 has-text-weight-semibold is-family-monospace mt-5 mb-3">
+        Cloud vSwitch
+      </h1>
+      <SignIn />
+    </div>
+  );
+}
+
+export default Entry;
---
+++ client/src/components/SignIn.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function SignIn() {
+  return (
+    <div className="mt-4">
+      <div className="field">
+        <div className="control has-icons-left">
+          <input className="input" type="email" placeholder="Email" />
+          <span className="icon is-small is-left">
+            <i className="fas fa-envelope" />
+          </span>
+        </div>
+      </div>
+      <div className="field">
+        <div className="control has-icons-left">
+          <input className="input" type="password" placeholder="Password" />
+          <span className="icon is-small is-left">
+            <i className="fas fa-lock" />
+          </span>
+        </div>
+      </div>
+      <div className="is-flex is-flex-direction-column">
+        <Link className="button is-info is-pulled-left my-2" to="/home">
+          Sign in
+        </Link>
+        <a
+          className="is-size-6"
+          href={`${process.env.WEB_APP_URL}/sign-up`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div>No account yet? Sign up here.</div>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
