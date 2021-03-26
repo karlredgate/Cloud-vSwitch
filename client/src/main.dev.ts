6cf4e95d9fdb26a96a6fed37d4311939feab260d
SiyanH <46579757+SiyanH@users.noreply.github.com>
2021-03-26 01:05:17 -0400

    Set min width and height for main window

--- client/src/main.dev.ts
+++ client/src/main.dev.ts
@@ -71,6 +71,8 @@ const createWindow = async () => {
     show: false,
     width: 1024,
     height: 728,
+    minWidth: 900,
+    minHeight: 500,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       nodeIntegration: true,
