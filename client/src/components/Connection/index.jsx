d0908f8fff0c37e736a4441ad016ccb757ab6728
SiyanH <46579757+SiyanH@users.noreply.github.com>
2021-03-25 22:47:16 -0400

    Fix position of connect section when showing more info

--- client/src/components/Connection/index.jsx
+++ client/src/components/Connection/index.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import Connect from './Connect';
 import ServerInfo from './ServerInfo';
+import styles from './index.css';
 
 function Connection() {
   const [showInfo, setShowInfo] = useState(false);
 
   return (
-    <div className="columns">
-      <div className="column">
+    <div>
+      <div className={showInfo ? styles.left : ''}>
         <Connect showInfo={showInfo} setShowInfo={setShowInfo} />
       </div>
       {showInfo && (
-        <div className="column">
+        <div className={styles.right}>
           <ServerInfo />
         </div>
       )}
