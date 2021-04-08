bee951e0b47de9d59f199ac5daca10a37f30fd96
SiyanH <46579757+SiyanH@users.noreply.github.com>
2021-04-08 01:17:14 -0400

    Protect private user routes

--- web/src/routes/MainRoutes.js
+++ web/src/routes/MainRoutes.js
@@ -2,18 +2,21 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "../components/Landing";
 import SignIn from "../components/auth/SignIn";
 import SignUp from "../components/auth/SignUp";
+import { ProvideAuth } from "../components/auth/user-auth";
 import UserRoutes from "./UserRoutes";
 
 function MainRoutes() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Landing} />
-        <Route path="/sign-in" component={SignIn} />
-        <Route path="/sign-up" component={SignUp} />
-        <Route component={UserRoutes} />
-      </Switch>
-    </Router>
+    <ProvideAuth>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Landing} />
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/sign-up" component={SignUp} />
+          <Route component={UserRoutes} />
+        </Switch>
+      </Router>
+    </ProvideAuth>
   );
 }
 
--- web/src/routes/UserRoutes.js
+++ web/src/routes/UserRoutes.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch, useLocation } from "react-router-dom";
 import "./UserRoutes.css";
 import Home from "../components/home";
 import Navbar from "../components/Navbar";
@@ -6,22 +6,37 @@ import Profile from "../components/profile";
 import CreateServer from "../components/home/CreateServer";
 import Organizations from "../components/organizations";
 import CreateOrganization from "../components/organizations/CreateOrganization";
+import { useAuth } from "../components/auth/user-auth";
 
 function UserRoutes() {
-  return (
-    <>
-      <Navbar />
-      <div className="container">
-        <Switch>
-          <Route path="/home" component={Home} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/organizations/new" component={CreateOrganization} />
-          <Route path="/organizations" component={Organizations} />
-          <Route path="/server/new" component={CreateServer} />
-        </Switch>
-      </div>
-    </>
-  );
+  const auth = useAuth();
+  const location = useLocation();
+
+  if (auth.user) {
+    return (
+      <>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/organizations/new" component={CreateOrganization} />
+            <Route path="/organizations" component={Organizations} />
+            <Route path="/server/new" component={CreateServer} />
+          </Switch>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <Redirect
+        to={{
+          pathname: "/sign-in",
+          state: { from: location },
+        }}
+      />
+    );
+  }
 }
 
 export default UserRoutes;
