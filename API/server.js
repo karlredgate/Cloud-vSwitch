6c48687e1d16fc90fa02aa030f09fc27d0ff7956
SiyanH <46579757+SiyanH@users.noreply.github.com>
2021-04-08 21:40:02 -0400

    Disable mock routes and relocate Firebase service account

--- API/server.js
+++ API/server.js
@@ -6,13 +6,13 @@ const path = require("path");
 
 // Firebase
 const admin = require("firebase-admin");
-const serviceAccount = require(process.env.SERVICE_ACCOUNT);
+const serviceAccount = require("./keys/cloud-vswitch-firebase-adminsdk.json");
 
 // Set up Firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-const db = admin.firestore();
+// const db = admin.firestore();
 
 // Sets up the Express App
 const app = express();
@@ -33,29 +33,29 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const users = [
-  {
-    id: 1,
-    userName: "yoda",
-    password: "YODA900",
-    email: "yoda@gmail.com",
-    org: "Star Wars",
-  },
-  {
-    id: 2,
-    userName: "darthmaul",
-    password: "DarthMaul200",
-    email: "darthmaul@gmail.com",
-    org: "Star Wars",
-  },
-  {
-    id: 3,
-    userName: "obiwankenobi",
-    password: "ObiWanKenobi55",
-    email: "obiwankenobi@gmail.com",
-    org: "Star Wars",
-  },
-];
+// const users = [
+//   {
+//     id: 1,
+//     userName: "yoda",
+//     password: "YODA900",
+//     email: "yoda@gmail.com",
+//     org: "Star Wars",
+//   },
+//   {
+//     id: 2,
+//     userName: "darthmaul",
+//     password: "DarthMaul200",
+//     email: "darthmaul@gmail.com",
+//     org: "Star Wars",
+//   },
+//   {
+//     id: 3,
+//     userName: "obiwankenobi",
+//     password: "ObiWanKenobi55",
+//     email: "obiwankenobi@gmail.com",
+//     org: "Star Wars",
+//   },
+// ];
 
 // Routes
 
@@ -63,26 +63,26 @@ const users = [
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "view.html")));
 
 // Get a collection of users
-app.get("/api/users", (req, res) => res.json(users));
+// app.get("/api/users", (req, res) => res.json(users));
 
 // Get a user by userId
-app.get("/api/users/:userId", (req, res) => {
-  const chosen = req.params.userId;
-
-  // console.log(chosen);
-
-  /* Check each character routeName and see if the same as "chosen"
-  If the statement is true, send the character back as JSON,
-  otherwise tell the user no character was found */
-
-  for (let i = 0; i < users.length; i++) {
-    if (chosen == users[i].id) {
-      return res.json(users[i]);
-    }
-  }
-
-  return res.json(false);
-});
+// app.get("/api/users/:userId", (req, res) => {
+//   const chosen = req.params.userId;
+//
+//   // console.log(chosen);
+//
+//   /* Check each character routeName and see if the same as "chosen"
+//   If the statement is true, send the character back as JSON,
+//   otherwise tell the user no character was found */
+//
+//   for (let i = 0; i < users.length; i++) {
+//     if (chosen == users[i].id) {
+//       return res.json(users[i]);
+//     }
+//   }
+//
+//   return res.json(false);
+// });
 
 // Create a new user
 app.post("/api/users", (req, res) => {
@@ -107,36 +107,36 @@ app.post("/api/users", (req, res) => {
 });
 
 // Update a user's info
-app.put("/api/users", (req, res) => {
-  const updatedUser = req.body;
-  for (let i = 0; i < users.length; i++) {
-    if (
-      users[i].id == updatedUser.id &&
-      users[i].userName === updatedUser.userName
-    ) {
-      users[i].password = updatedUser.password;
-      users[i].email = updatedUser.email;
-      users[i].org = updatedUser.org;
-      return res.json(updatedUser);
-    }
-  }
-  return res.json(false);
-});
+// app.put("/api/users", (req, res) => {
+//   const updatedUser = req.body;
+//   for (let i = 0; i < users.length; i++) {
+//     if (
+//       users[i].id == updatedUser.id &&
+//       users[i].userName === updatedUser.userName
+//     ) {
+//       users[i].password = updatedUser.password;
+//       users[i].email = updatedUser.email;
+//       users[i].org = updatedUser.org;
+//       return res.json(updatedUser);
+//     }
+//   }
+//   return res.json(false);
+// });
 
 // Delete a user
-app.delete("/api/users", (req, res) => {
-  const deletedUser = req.body;
-  for (let i = 0; i < users.length; i++) {
-    if (
-      users[i].id == deletedUser.id &&
-      users[i].userName === deletedUser.userName
-    ) {
-      delete users[i];
-      return res.json(deletedUser);
-    }
-  }
-  return res.json(false);
-});
+// app.delete("/api/users", (req, res) => {
+//   const deletedUser = req.body;
+//   for (let i = 0; i < users.length; i++) {
+//     if (
+//       users[i].id == deletedUser.id &&
+//       users[i].userName === deletedUser.userName
+//     ) {
+//       delete users[i];
+//       return res.json(deletedUser);
+//     }
+//   }
+//   return res.json(false);
+// });
 
 // Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
